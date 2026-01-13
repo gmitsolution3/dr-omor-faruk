64988020d48c99065aa86d3f8deb7cef706ec373
gmitsolution3 <gmitsolution3@gmail.com>
2026-01-13 16:17:31 +0600

    update

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -14,7 +14,7 @@ export const router = createBrowserRouter([
       },
       {
         path: "/doctor-profile",
-        element: <DoctorProfile />,
+        element: <DoctorProfile />, 
       },
     ],
   },
